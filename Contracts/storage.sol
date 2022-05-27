d2d80b583d4b5be7e37d42735d7fbdd8507cab6c
"Barath kanna" <"barathas12@gmail.com">
2022-05-27 16:55:19 +0530

    Update store function

--- Contracts/storage.sol
+++ Contracts/storage.sol
@@ -2,5 +2,11 @@
 pragma solidity >=0.8.7 <0.9.0;
 
 contract Storage {
-    
+     uint32 FavNumber;
+ 
+ function store(uint32 number) public {
+    FavNumber = number;
+ }
 }
+
+// 0xd9145CCE52D386f254917e481eB44e9943F39138 - Contract address
