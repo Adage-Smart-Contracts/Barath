f4347b25f99d25c5e0a7f8f6031e6081bc393d8c
"Barath kanna" <"barathas12@gmail.com">
2022-05-27 17:21:44 +0530

    update struct storage

--- Contracts/storage.sol
+++ Contracts/storage.sol
@@ -3,11 +3,17 @@ pragma solidity >=0.8.7 <0.9.0;
 
 contract Storage {
      uint32 public FavNumber;
+     person public Barath = person({name: "Barath Kanna",age: 22});
+
+ struct person {
+     string name;
+     uint32 age;
+ }
  
  function store(uint32 number) public {
     FavNumber = number;
  }
-
+//  view and pure functions don't spend gas when executed
  function retrieve() public view returns(uint32) {
      return FavNumber;
  }
