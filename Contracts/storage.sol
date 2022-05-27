7be214f72bd679cf2b47cda2b4b8f3c0ab556077
"Barath kanna" <"barathas12@gmail.com">
2022-05-27 17:10:13 +0530

    Add a function to retrieve stored value

--- Contracts/storage.sol
+++ Contracts/storage.sol
@@ -7,6 +7,10 @@ contract Storage {
  function store(uint32 number) public {
     FavNumber = number;
  }
+
+ function retrieve() public view returns(uint32) {
+     return FavNumber;
+ }
 }
 
 // 0xd9145CCE52D386f254917e481eB44e9943F39138 - Contract address
